5700b47b786c27a48de15b7458aa3906b13b809a
Cyrus Chan <kb041204@hotmail.com>
2025-09-06 11:25:50 +0800

    Send the first article from meethk to channel

--- discord_bot.py
+++ discord_bot.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+from classes import meethk_scrapper
 
 # Load environment variables
 load_dotenv()
@@ -11,6 +12,14 @@ intents.message_content = True  # Required for message content access
 
 bot = discord.Client(intents=intents)
 
+def trimText(text, max_line=10):
+    lines = text.splitlines()
+    num_line = min(max_line, len(lines))
+    trimmed_text = '\n'.join(lines[:num_line])
+    if len(lines) > num_line:
+        trimmed_text = trimmed_text + "\n..."
+    return trimmed_text
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
@@ -20,7 +29,12 @@ async def on_ready():
     channel = bot.get_channel(channel_id)
     
     if channel:
-        await channel.send("Hello! This is a test message from our bot!")
+        articles = meethk_scrapper.getMeetHKArticleObjects()
+        latest_notice_title = articles[0].title
+        notice_content = trimText(articles[0].postContentPureText, 10)
+        notice_url = articles[0].postLink
+        message = discord.Embed(title=str(latest_notice_title), description=str(notice_content), url=str(notice_url), colour=discord.Color.teal())
+        await channel.send(embed=message)
     else:
         print(f"Channel with ID {channel_id} not found")
 
